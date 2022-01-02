77e6aed2d79037e5dafead9ceb54028a7b3ca10f
ymtam <ymtam81@yahoo.com>
2022-01-02 20:49:52 +0800

    main

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-import kivy
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 
-class FunkyButton(Button):
-    pass
+class LoginScreen(GridLayout):
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text='User Name'))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.add_widget(Label(text='password'))
+        self.password = TextInput(password=True, multiline=False)
+        self.add_widget(self.password)
 
 class DepotApp(App):
     def build(self):
-        return FunkyButton(
-            pos=(100,100),
-            size=(500,500),
-            size_hint=(None,None)
-        )
+        return LoginScreen()
+
 
 if __name__ == '__main__':
     DepotApp().run()
